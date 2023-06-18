a175533c3972bc7cd6013d9bfd0b66777199e5ea
rzzkv <rzzkv@yandex.ru>
2023-06-18 07:05:27 +0500

    s

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,16 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
+        printMessage(20);
+    }
 
+    private void printMessage(int n) {
+        if (n <= 0) {
+            return;
+        }
+
+        System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+
+        printMessage(n - 1);
     }
 }
